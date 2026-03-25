3344ee162a44cade19c67dc47f10655c546a14a1
Xaldin83 <mariuslucas.pichon@gmail.com>
2026-03-25 19:54:35 +0100

    Rajout de boutons d'accès aux univers depuis l'accueil

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,4 +1,8 @@
 import Navbar from "../../components/hearder"
+import { Link } from "react-router-dom";
+
+import styles from './styles/Home.module.css'  // ← import CSS Module
+
 
 
 document.title="Villainous"
@@ -6,7 +10,15 @@ export default function Home(){
     return(
         <>
         <Navbar/>
-        <h1>Bienvenue</h1>
+        <h1 className={styles.h1}>Bienvenue</h1>
+
+        <h2 className={styles.h2}>Veuillez selectionner votre univers</h2>
+
+        <div className={styles.buttonMenus}>
+            <Link to="/disney" className={styles.Disney}><img src="./img/Logo.webp" alt="Logo Disney Villainous" /></Link>
+            <Link to="/marvel" className={styles.Marvel}><img src="./img/Logo_Marvel.webp" alt="Logo Marvel Villainous" /></Link>
+            <Link to="/starwars" className={styles.StarWars}><img src="./img/Logo_Star_Wars.webp" alt="Logo Star Wars Villainous" /></Link>
+        </div>
         </>
     )
 }
