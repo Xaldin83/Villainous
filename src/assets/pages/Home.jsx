6364a7ddff9e9ea7e270d67c9e96d3a52cdbc5f2
Xaldin83 <mariuslucas.pichon@gmail.com>
2026-05-10 11:34:16 +0200

    Modification de la responsive des boites

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -9,15 +9,17 @@ document.title="Villainous"
 export default function Home(){
     return(
         <>
-        <Navbar/>
-        <h1 className={styles.h1}>Bienvenue</h1>
+        <div className={styles.pageWrapper}>
+            <Navbar/>
+            <h1 className={styles.h1}>Bienvenue</h1>
 
-        <h2 className={styles.h2}>Veuillez selectionner votre univers</h2>
+            <h2 className={styles.h2}>Veuillez selectionner votre univers</h2>
 
-        <div className={styles.buttonMenus}>
-            <Link to="/disney" className={styles.Disney}><img src="./img/Logo.webp" alt="Logo Disney Villainous" /></Link>
-            <Link to="/marvel" className={styles.Marvel}><img src="./img/Logo_Marvel.webp" alt="Logo Marvel Villainous" /></Link>
-            <Link to="/starwars" className={styles.StarWars}><img src="./img/Logo_Star_Wars.webp" alt="Logo Star Wars Villainous" /></Link>
+            <div className={styles.buttonMenus}>
+                <Link to="/disney" className={styles.Disney}><img src="./img/Logo.webp" alt="Logo Disney Villainous" /></Link>
+                <Link to="/marvel" className={styles.Marvel}><img src="./img/Logo_Marvel.webp" alt="Logo Marvel Villainous" /></Link>
+                <Link to="/starwars" className={styles.StarWars}><img src="./img/Logo_Star_Wars.webp" alt="Logo Star Wars Villainous" /></Link>
+            </div>
         </div>
         </>
     )
